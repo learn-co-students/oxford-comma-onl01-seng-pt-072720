4f7467c29577f616d6bd0869d99fdfa152075703
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-07-30 06:33:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size < 2 
+    return array.join("")
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    word = array[-1]
+    array[-1] = "and #{word}"
+    return array.join(", ")
+  end
 end
