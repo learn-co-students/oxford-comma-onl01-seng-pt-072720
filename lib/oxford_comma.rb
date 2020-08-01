1802141a9acef815b7f7d9023b21e5bb42b6dcf8
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-01 21:35:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+if array.length == 1
+  array.join
+elsif array.length == 2
+  last_item = "and " + array.pop
+  array << last_item
+  array.join(" ")
+else array.length > 2
+  last_item = "and " + array.pop
+  array << last_item
+  array.join(", ")
+end
 end
