28f17e92c9efb5db92b72bb3c2fc92a4ee4def50
Brian Donnoe <dunow33@hotmail.com>
2020-08-03 04:18:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+def oxford_comma(arrayArg)
+  if(arrayArg.size == 1)
+   return arrayArg.join
+  elsif(arrayArg.size == 2)
+   return arrayArg.join(" and ")
+  else
+    last = (arrayArg.length - 1)
+    temp = arrayArg[last]
+    arrayArg[last] = "and " + temp
+    return arrayArg.join(", ")
+  end
 end
