ea80a6bfd35fdc261b40de830bd4049cfa7254f9
Kelin Field <kfield111@hotmail.com>
2020-08-04 00:28:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+  if array.length > 3
+    array[-1] = "and #{array[-1]}"
+    array.join (", ")
+  elsif array.length == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else array.join (" and ")
+  end
 
 end
