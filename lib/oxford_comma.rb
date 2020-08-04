fbf2df655dc7fd087275382b86f67b7174dfb1fb
Mike Shahindoust <gobias101@gmail.com>
2020-08-04 00:00:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+if array.length == 1
+  string = array.join
+  return string
+elsif array.length == 2
+  string = array.join(" and ")
+  return string
+elsif array.length >= 3
+  last_index = array.pop
+  string = array.join(", ") + ", and #{last_index}"  
+  return string
+end
 end
