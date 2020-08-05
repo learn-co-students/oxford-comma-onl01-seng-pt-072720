6be3e88266d01e89d18fa4ffc1296ef453f61f7c
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-05 18:50:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2 
+    return array[0..1].join(" and ")
+  else
+#   
+    new_array = array[0..-2] 
+    return new_array.join(", ") + ", and " + array[-1]
+  end
+end 
 
-end
+  # array.join(',')
+#     array.join(", ")
+#     array.insert(-2, " and ")
+#   end
+# end
